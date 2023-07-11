ee3ec44607d3100e0f04f933a606dbd54ff83f33
crunky78 <sung6194@naver.com>
2023-07-11 20:47:15 +0900

    1주차

---
+++ javascript/현화/examples/part1/006.js
@@ -0,0 +1,16 @@
+(3+12)/5
+declaredVariable
+greeting("hello")
+
+if(true){
+}
+
+function greeting(){
+    "hello"
+    "Chloe"+3
+    greeting()
+}
+
+greeting(if(true){
+    console.log("It is not acceptable")
+})
